846c408f0fe2ff5af4cf6c0e29e24506568b03c0
christina <chrstnaheadley@gmail.com>
2021-05-12 13:14:50 -0400

    adds Input and Output exercises

--- Input-and-Output/age.js
+++ Input-and-Output/age.js
@@ -8,7 +8,10 @@
 // In 40 years, you will be 60 years old.
 
 // let age = 20;
-// console.log(`In 10 years, you will be ${age + 10} years old.`);
+let rlSync = require("readline-sync");
+let age = Number(rlSync.question("How old are you?"));
+console.log(`You are ${age}.`);
+console.log(`In 10 years, you will be ${age + 10} years old.`);
 // > {
 //   ...   let foo = 'bar';
 //   ... }
